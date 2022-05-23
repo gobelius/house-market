ba161fa83560e8eb4eccbb6d986804accf50f075
gobelius <bego.alibasic+1@gmail.com>
2022-05-24 01:59:27 +0300

    add listing

--- src/pages/Offers.js
+++ src/pages/Offers.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import {
   collection,
   getDocs,
@@ -17,7 +16,6 @@ import ListingItem from "../components/ListingItem";
 const Offers = () => {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
-  const params = useParams();
   useEffect(() => {
     const fetchListings = async () => {
       try {
